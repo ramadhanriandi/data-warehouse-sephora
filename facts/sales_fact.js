0ee30f9e2a65f4a72cc595cae1c5a8032ff3170b
ramadhanriandi <izurbudianto@gmail.com>
2021-04-05 08:06:00 +0700

    Populate sales fact table

--- facts/sales_fact.js
+++ facts/sales_fact.js
@@ -1,65 +1,58 @@
 require('dotenv').config();
 const faker = require('faker');
-const { DELIVERIES_BY_COUNTRY } = require('./data/deliveries');
-const models = require('./models');
+const models = require('../models');
 
 const findSalesDimensions = async () => {
-    const times = await models.TimeDimension.findAll({ attributes: ["id"] });
-    const products = await models.ProductDimension.findAll({ attributes: ["id", "unit_price", "unit_cost"] });
-    const outlets = await models.OutletDimension.findAll({ attributes: ["id", "country"] });
-    const promotions = await models.PromotionDimension.findAll({ attributes: ["id", "start_date", "end_date", "discount"] });
-    const deliveries = await models.DeliveryDimension.findAll({ attributes: ["id", "name"] });
+    const dates = await models.DateDimension.findAll({ attributes: ["id"] });
+    const products = await models.ProductDimension.findAll({ attributes: ["id", "price_per_unit", "cost_per_unit"] });
+    const outlets = await models.OutletDimension.findAll({ attributes: ["id"] });
+    const customers = await models.CustomerDimension.findAll({ attributes: ["id"] });
+    const promotions = await models.PromotionDimension.findAll({ attributes: ["id", "discount"] });
 
-    return { times, products, outlets, promotions, deliveries };
+    return { dates, products, outlets, customers, promotions };
 };
 
 (async () => {
     // SALES FACT
     await models.SalesFact.sync({ force: true });
     await findSalesDimensions().then((salesDimensions) => {
-        const { times, products, outlets, promotions, deliveries } = salesDimensions;
+        const { dates, products, outlets, customers, promotions} = salesDimensions;
 
         outlets.forEach((outlet) => {
             (async () => {
-                for (let timeId = 1; timeId <= times.length; timeId++) {
-                    const numberOfTransactions = faker.random.number({ min: 3, max: 10 });
+                for (let dateId = 1; dateId <= dates.length; dateId++) {
+                    const numberOfTransactions = faker.random.number({ min: 2, max: 5 });
 
                     for (let transactionId = 0; transactionId < numberOfTransactions; transactionId++) {
-                        const paymentId = faker.random.number({ min: 1, max: 5 });
                         const numberOfItems = faker.random.number({ min: 1, max: 3 });
-                        let deliveryId = 1;
-                        const transactionNumber = `STARBUCKS-${outlet.id}-${timeId}-${transactionId}`;
-
-                        if (faker.random.number({ min: 1, max: 3 }) % 2 === 0) {
-                            const deliveriesInCountry = DELIVERIES_BY_COUNTRY[outlet.country];
-                            const deliveryName = deliveriesInCountry[faker.random.number({ max: deliveriesInCountry.length - 1 })];
-                            const choosenDelivery = deliveries.find((delivery) => {
-                                return delivery.name === deliveryName;
-                            });
-
-                            deliveryId = choosenDelivery.id;
-                        }
+                        const customer = customers[faker.random.number({ max: customers.length - 1 })];
+                        const transactionNumber = `SEPHORA-${outlet.id}-${dateId}-${transactionId}`;
 
                         for (let productIdx = 0; productIdx < numberOfItems; productIdx++) {
                             const product = products[faker.random.number({ max: products.length - 1 })];
-                            const promotion = productIdx === 0 ? promotions[faker.random.number({ max: promotions.length - 1 })] : promotions[0];
+                            let promotion = productIdx === 0 ? promotions[faker.random.number({ max: promotions.length - 1 })] : promotions[0];
                             const unitsSold = faker.random.number({ min: 1, max: 2 });
-                            let dollarsSold = ((100 - promotion.discount) / 100) * product.unit_price;
+                            let dollarsSold = ((100 - promotion.discount) / 100) * product.price_per_unit;
 
                             if (unitsSold === 2) {
-                                dollarsSold += product.unit_price;
+                                dollarsSold += product.price_per_unit;
                             }
 
-                            const dollarsCost = product.unit_cost * unitsSold;
-                            const profit = dollarsSold - dollarsCost;
+                            const dollarsCost = product.cost_per_unit * unitsSold;
+                            let profit = dollarsSold - dollarsCost;
+
+                            if (profit < 0) {
+                                promotion = promotions[0];
+                                dollarsSold = unitsSold * product.price_per_unit;
+                                profit = dollarsSold - dollarsCost;
+                            }
 
                             let sales = models.SalesFact.build({
-                                time_id: timeId,
+                                date_id: dateId,
                                 product_id: product.id,
                                 outlet_id: outlet.id,
-                                payment_id: paymentId,
+                                customer_id: customer.id,
                                 promotion_id: promotion.id,
-                                delivery_id: deliveryId,
                                 transaction_number: transactionNumber,
                                 units_sold: unitsSold,
                                 dollars_sold: dollarsSold,
@@ -70,7 +63,6 @@ const findSalesDimensions = async () => {
                             await sales.save();
                         }
                     }
-                    await console.log(timeId);
                 }
             })();
         });
